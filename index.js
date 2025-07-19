8917ae79fc7bdd7e823c911d91bf17565953e221
Garandov <hroldima30@gmail.com>
2025-07-19 21:00:43 +0300

    Подключил библиотеку Path

--- index.js
+++ index.js
@@ -9,10 +9,15 @@ import UserModel from './models/User.js';
 import checkAuth from './utilis/checkAuth.js';
 import * as userControler from './controlers/userControler.js'
 import * as PostControler from './controlers/PostControlers.js'
+import path from 'path';
+import { fileURLToPath } from 'url';
 import handleerrors from './utilis/handleerrors.js';
 import cors from 'cors';
 import dotenv from "dotenv";
 dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const PORT = process.env.PORT;
 const DB = process.env.DB;
@@ -40,6 +45,12 @@ app.get('/post/:id',PostControler.getOne)
 app.post('/create-post', checkAuth,postCreateValidation,handleerrors,PostControler.create)
 app.delete('/post/:id', checkAuth,PostControler.remove)
 app.patch('/post/:id', checkAuth,postCreateValidation,handleerrors,PostControler.update)
+
+app.use(express.static(path.join(__dirname, 'frontend', 'build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
+});
 
 app.listen(PORT,() => {
   try {
